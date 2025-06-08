0447afe0b71b60c9d45687faab646e4cb18d480d
Christian <b.christian.fuentes@gmail.com>
2025-06-08 14:33:17 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+</style>
