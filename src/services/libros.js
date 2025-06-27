3df3d367a14ef13805efe7b42d095b95b6f7f851
MarcosGabriel-01 <marcos.castrogiovani@hotmail.com>
2025-06-26 23:54:34 -0300

    conectando a mockapi

--- src/services/libros.js
+++ src/services/libros.js
@@ -1,7 +1,19 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.example.com/libros'; // Replace with your actual API URL
+// const API_URL = 'https://64f8b230824680fd218f9e5f.mockapi.io/api/libros'; 
+const API_URL = 'https://mockapi.io/projects/684b6a3b165d05c5d35c6703';
 
+
+
+export const guardarLibro = async (libro) => {
+    try {
+        const response = await axios.post(API_URL, libro);
+        return response.data;
+    } catch (error) {
+        console.error('❌ Error al guardar el libro:', error);
+        throw error;
+    }
+};
 export const getLibros = async () => {
     try {
         const response = await axios.get(API_URL);
@@ -10,7 +22,7 @@ export const getLibros = async () => {
         console.error('Error al obtener los libros:', error);
         throw error;
     }
-}
+};
 
 export const getLibroPorId = async (id) => {
     try {
@@ -20,4 +32,37 @@ export const getLibroPorId = async (id) => {
         console.error(`Error al obtener el libro con ID ${id}:`, error);
         throw error;
     }
-}
+};
+
+export const crearLibro = async (nuevoLibro) => {
+    try {
+        const response = await axios.post(API_URL, nuevoLibro);
+        return response.data;
+    } catch (error) {
+        console.error('Error al crear el libro:', error);
+        throw error;
+    }
+};
+
+export const actualizarLibro = async (id, libroActualizado) => {
+    try {
+        const response = await axios.put(`${API_URL}/${id}`, libroActualizado);
+        return response.data;
+    } catch (error) {
+        console.error('Error al actualizar el libro:', error);
+        throw error;
+    }
+};
+
+export const eliminarLibro = async (id) => {
+    try {
+        const response = await axios.delete(`${API_URL}/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error al eliminar el libro:', error);
+        throw error;
+    }
+
+    
+};
+
